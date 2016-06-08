f4decec1c604abdf543ea33b6c473e2dd52cf1ae
abidog <346montgomery@gmail.com>
2016-06-07 19:19:15 -0700

    Solution SRM 691

---
+++ SRM 691 DIV 2/src/Plusonegame.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+import static java.lang.Math.*;
+import java.util.*;
+
+
+public class Plusonegame {
+	
+	public String getorder(String s) {
+
+        return null;
+	}
+}
