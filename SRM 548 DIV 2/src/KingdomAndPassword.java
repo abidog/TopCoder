bea34c2eac0f4048821f3b00d11e5bee3d8f3f27
abidog <346montgomery@gmail.com>
2016-06-15 17:20:29 -0700

    Solution SRM548

---
+++ SRM 548 DIV 2/src/KingdomAndPassword.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.*;
+import static java.lang.Math.*;
+import java.util.*;
+
+
+public class KingdomAndPassword {
+	
+	public long newPassword(long oldPassword, int[] restrictedDigits) {
+		return 0;
+	}
+}
